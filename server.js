b6eb3b92794e0803f5e44f6f8f4388fd99860f98
Andrew Prine <andrewpprine@gmail.com>
2018-09-17 18:32:38 -0700

    var port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+var port = 3000;
 
 app.use(express.static("public"));
 
@@ -8,8 +9,8 @@ app.get("/testing", function(req,res){
 	res.send("Hello world!");
 })
 
-app.listen(3000, function(){
-	console.log('listening on 3000');
+app.listen(port, function(){
+	console.log('listening on port:'+ port);
 });
 
 
